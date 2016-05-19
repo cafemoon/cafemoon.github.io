57bb1a7e6fe7343e699b6d7e9ddf8d036eaa5f3b
kawabata.satoshi <kawabata.satoshi@artspark.co.jp>
2016-05-19 15:25:32 +0900

    空を追加

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     var HEIGHT, WIDTH;
     var hemisphereLight, shadowLight;
     
-    var sea, airplane;
+    var sea, sky, airplane;
     
     var Colors = {
         red: 0xf25346,
@@ -29,7 +29,47 @@
         this.mesh = new THREE.Mesh(geom, mat);
         this.mesh.receiveShadow = true;
     };
-    
+
+    var Cloud = function() {
+        this.mesh = new THREE.Object3D();
+        var geom = new THREE.BoxGeometry(20, 20, 20);
+        var mat = new THREE.MeshPhongMaterial({
+            color: Colors.white
+        });
+        var nBlocs = 3 + Math.floor(Math.random() * 3);
+        for (var i=0; i<nBlocs; i++) {
+            var m = new THREE.Mesh(geom, mat);
+            m.position.x = i * 15;
+            m.position.y = Math.random() * 10;
+            m.position.z = Math.random() * 10;
+            m.rotation.z = Math.random() * Math.PI * 2;
+            m.rotation.y = Math.random() * Math.PI * 2;
+            var s = 0.1 + Math.random() * 0.9;
+            m.scale.set(s, s, s);
+            m.castShadow = true;
+            m.receiveShadow = true;
+            this.mesh.add(m);
+        }
+    };
+
+    var Sky = function() {
+        this.mesh = new THREE.Object3D();
+        this.nClouds = 20;
+        var stepAngle = Math.PI*2 / this.nClouds;
+        for (var i=0; i<this.nClouds; i++) {
+            var c = new Cloud();
+            var a = stepAngle * i;
+            var h = 750 + Math.random() * 200;
+            c.mesh.position.y = Math.sin(a) * h;
+            c.mesh.position.x = Math.cos(a) * h;
+            c.mesh.rotation.z = a + Math.PI/2;
+            c.mesh.position.z = -400 - Math.random() * 400;
+            var s = 1 + Math.random() * 2;
+            c.mesh.scale.set(s, s, s);
+            this.mesh.add(c.mesh);
+        }
+    };
+
     function doWindowResize() {
         HEIGHT = window.innerHeight;
         WIDTH = window.innerWidth;
@@ -96,9 +136,16 @@
         sea.mesh.position.y = -600;
         scene.add(sea.mesh);
     }
+
+    function createSky() {
+        sky = new Sky();
+        sky.mesh.position.y = -600;
+        scene.add(sky.mesh);
+    }
     
     function loop() {
         sea.mesh.rotation.z += 0.005;
+        sky.mesh.rotation.z += 0.01;
         renderer.render(scene, camera);
         requestAnimationFrame(loop);
     }
@@ -107,6 +154,7 @@
         createScene();
         createLights();
         createSea();
+        createSky();
         loop();
     }
     
